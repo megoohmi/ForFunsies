a197369d04eeeccbb721b51c799d962b85a85ab4
Celine Vergara <111476474+megoohmi@users.noreply.github.com>
2022-09-19 10:56:43 +0800

    Switch

---
+++ week6/switch/App.java
@@ -0,0 +1,34 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        String today = "monday";
+
+        switch (today) {
+            case "monday":
+                System.out.println("monday nanaman?!");
+                break;
+            case "tuesday":
+                System.out.println("may 7am class nanaman.. ahuhu");
+                break;
+            case "wednesday":
+                System.out.println("YES REST DAY!!! or maybe not..");
+                break;
+            case "thursday":
+                System.out.println("HUHUHU THURS NA PERO auco na rold");
+                break;
+            case "friday":
+                System.out.println("hindi na masaya fridays pls lng");
+                break;
+            case "Saturday":
+                System.out.println("MAY PASOK PA RIN?!");
+                break;
+            case "Sunday":
+                System.out.println("i think finally??? tulog time");
+                break;
+            default:
+                System.out.println("naiiyak na koh");
+                break;
+        }
+
+    }
+}
