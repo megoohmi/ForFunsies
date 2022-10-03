0fa1b7957c9483d09d03dd3330026ee7fe266f5a
Celine Vergara <111476474+megoohmi@users.noreply.github.com>
2022-10-03 10:37:53 +0800

    Create App.java

---
+++ week 8/App.java
@@ -0,0 +1,19 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        for (int firstNameCount = 5; firstNameCount != 0; firstNameCount--) {
+            System.out.println("Celine Margarette");
+        }
+        int surNameCount = 5; 
+        do {
+            System.out.println("Vergara");
+            surNameCount--;
+        } while (surNameCount != 0);
+        
+         int nickNameCount = 5;
+        while (nickNameCount != 0) {
+            System.out.println("Meg");
+            nickNameCount--;
+        }
+    }
+}
